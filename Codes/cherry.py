e0127a07d6584a52d863f0231ee7ea7d5e5325c1
Nowar Alashkar <nowarashkar@gmail.com>
2023-08-08 20:23:24 -0400

    Added virtual environment (CherryVenv)

---
+++ Codes/cherry.py
@@ -0,0 +1,23 @@
+#imports of outside libraries
+import numpy as np
+import matplotlib.pyplot as plt 
+#----------
+inp=input('what # am i thinking of from 1-10:')
+if inp==5:
+    print('True')
+else:
+    print('False')    
+    k=input('try again:')
+    if k==5:
+        print('True')
+    else:
+        print('False')
+if inp == 4:
+    print('Very Hot')
+else:
+    print ('False')
+
+x=np.array([1,2,3,5,7,3])
+y=np.array([3,2,4,5,3,2])
+plt.plot(x,y)
+plt.show(3)
--- cherry.py
+++
@@ -1,7 +0,0 @@
-inp = ('What color am I thinking of?')
-
-if inp == 2:
-    print('False')
-else:
-    print('True')
-intg = 3.2 
