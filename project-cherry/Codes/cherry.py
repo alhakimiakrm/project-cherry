8ad2373dd2ae1f98b37e8a1cca39a8f0e333e518
akrm al-hakimi <alhakimiakrmj@gmail.com>
2023-08-12 19:16:15 -0400

    .\cherry.py

--- project-cherry/Codes/cherry.py
+++ project-cherry/Codes/cherry.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import random 
 import pygame
 import pymunk
+import pymunk.pygame_util
 #PYGAME COLORS-------------------------
 white =     ( 255, 255, 255 )
 blue =      ( 0, 0, 255 )
@@ -18,92 +19,86 @@ black=      ( 0, 0, 0 )
 # is affected by time and other factors
 # tends to disappear after a certain amount of time
 # can also be expanded, changed (in shape and mass) and destroyed
-
 #---------------------------------------------------------------
 
-#OUR WINDOW (SIZE & COORDINATES)--------------------------------
+pygame.init()
+
 WIDTH, HEIGHT = 800, 800
-botleft=0,0             #these coordinates are based of Pymunk's coordinates
-botright=WIDTH,HEIGHT    
-topleft=0,HEIGHT 
-topright=WIDTH,0
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-#PHYSICS SPACE--------------------------------------------------
-space=pymunk.Space() #All the physics will be inside this 'space'
-space.gravity=0,-200 #this gives gravity to our space in the negative y direction
+def draw(space, window, draw_options): #defining our draw options within our space so we can utilize pygame within pymunk and vice versa
+    window.fill("black")
+    space.debug_draw(draw_options)
+    pygame.display.update()
+    
+def boundary_set(space, width, height): #same output of code, different format for more precise addition and change of code
+    rects = [
+        [(width/2, height - 10), (width, 20)],
+        [(width/2, 10), (width, 20)],
+        [(10, height/2), (20, height)],
+        [(width - 10, height/2), (20, height)]
+    ]
+    
+    for pos, size in rects: #we create a "body" for the border that is static so we can also add things to the border that may affect the particles state (such as the elasticity and friction of it)
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.elasticity = 0.5  #less elasticity on border will result in a drop off in "bounciness"
+        shape.friction = .5
+        space.add(body, shape)
+        
+        
+    
+    
+def create_cherry(space, radius, mass): #I created cherry within a function so that we can call this function when we add new elements to the simulation later on as opposed to rewriting hr attributes each time 
+    cherry = pymunk.Body()
+    cherry.position = (400, 400) 
+    shape = pymunk.Circle(cherry, radius)
+    shape.mass = mass 
+    shape.elasticity = 1
+    shape.friction = 0.5 
+    shape.color = (255, 0, 0, 75)
+    space.add(cherry, shape)
+    return shape 
+    
 
-#PARTICLE1-CHERRY-----------------------------------------------
-cherry=pymunk.Body() #creating Cherry's body, no argument is added which gives us a dynamic body that reactes to forces,collisions etc.
-cherry.position=400,400 #Initial position of Cherry
-cherry_shape=pymunk.Circle(cherry,12) # second argument is the radius
-cherry_shape.mass=1
-cherry_shape.density=1
-cherry_shape.elasticity=1 # '1' is perfect elasticity
-space.add(cherry,cherry_shape) # we must add out body and shape to our physics space (body,shape)
-
-#CONVERSION OF COORDINATES-------------
-def convert_coordinates(point):
-    return point[0], HEIGHT-point[1]
-
-#WINDOW BARRIER-------------------------------------------------
-#Bottom---------
-barrier_body_bottom=pymunk.Body(body_type=pymunk.Body.STATIC) #This creates a static body 
-barrier_shape_bottom=pymunk.Segment(barrier_body_bottom,(0,0),(800,0),0) #This defines the shape of our body to be a Segment from point (0,0) to (800,0). 3rd argument is the width of the Segment
-barrier_shape_bottom.elasticity=1 #This gives our shape elasticity,'1' being perfectly elastic 
-space.add(barrier_body_bottom,barrier_shape_bottom)#Then we MUST add our body and its shape to our physics space (body,shape)
-#Top------------
-barrier_body_top=pymunk.Body(body_type=pymunk.Body.STATIC) 
-barrier_shape_top=pymunk.Segment(barrier_body_top,(0,800),(800,800),0)  
-barrier_shape_top.elasticity=1 
-space.add(barrier_body_top,barrier_shape_top) 
-#Left-----------
-barrier_body_left=pymunk.Body(body_type=pymunk.Body.STATIC)
-barrier_shape_left=pymunk.Segment(barrier_body_left,(0,0),(0,800),0)
-barrier_shape_left.elasticity=1
-space.add(barrier_body_left,barrier_shape_left)
-#Right----------
-barrier_body_right=pymunk.Body(body_type=pymunk.Body.STATIC)
-barrier_shape_right=pymunk.Segment(barrier_body_left,(800,0),(800,800),0)
-barrier_shape_right.elasticity=1
-space.add(barrier_body_right,barrier_shape_right)
-#----------------------------------------------------------------
-
-#SETUP VISUALIZATION VIA WINDOW----------------------------------
-pygame.init ()
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Simulation Project")
-BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
-#CLOCKS----------------------
-clock = pygame.time.Clock()
-FPS=60
-#-----------------------------
-
-def main():
+def run (window, width, height): #just redid our main loop 
     run = True
+    clock = pygame.time.Clock()
+    fps = 60
+    dt = 1 / fps
 
+    space = pymunk.Space()
+    space.gravity = (0, 981) #9.81 m/s gravitational constant
+    
+    cherry = create_cherry(space, 30, 10)
+    boundary_set(space, width, height)
+    
+    draw_options = pymunk.pygame_util.DrawOptions(window)
+    
+    
     while run:
-        WIN.fill(black)
-        x,y=convert_coordinates(cherry.position)
-        pygame.draw.circle(WIN,white,(int(x),int(y)),12)
-        
-       
-    
-        
-        clock.tick(FPS)
-        space.step(1/FPS)
-        pygame.display.update()
-    
-        
-        
-        for event in pygame.event.get():#initializing user input for when the window is closed out (x) 
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False 
+                run = False
+                break
             
-                
+            
+        draw(space, window, draw_options)
+        space.step(dt)
+        clock.tick(fps)
+        
     pygame.quit()
     
-    
-main()
-#we call main which means this window can only be initialized within VScode, not outside. 
-#-------------
+if __name__ == "__main__":
+    run(window, WIDTH, HEIGHT)
+            
+
+
+
+#I'm assuming you meant to leave this as just a reference so I commented them out but I don't know
+#botleft=0,0            #these coordinates are based of Pymunk's coordinates 
+#botright=WIDTH,HEIGHT    
+#topleft=0,HEIGHT 
+#topright=WIDTH,0
 
